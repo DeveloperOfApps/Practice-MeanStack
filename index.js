8e2bc677436ecf6825098c9cf009bc22c49b93e9
croblesl <croblesl@bancochile.cl>
2017-08-16 16:01:24 -0300

    Primer commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 /**
  * Created by Imagemaker on 16/08/2017.
  */
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+    if(err){
+        console.log("Could not connect to database: ", err);
+    }else{
+        console.log(config.secret);
+        console.log("Connected to database: " + config.db);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World </h1><h2>Hello Efrain</h2>');
+});
+
+app.listen(8000, () => {
+    console.log("Listening on port 8080");
+});
